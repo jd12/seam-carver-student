3b958ba75814d08fe80f3d4cba7a2b1729adc0da
J.D. DeVaughn-Brown <jd@cs.umass.edu>
2018-01-17 13:19:02 -0800

    Initial Commit

---
+++ src/test/java/SeamCarverTest.java
@@ -0,0 +1,52 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SeamCarverTest {
+
+  @Test
+  public void testSeamCarver() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testPicture() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testWidth() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testHeight() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testEnergy() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testFindHorizontalSeam() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testFindVerticalSeam() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testRemoveHorizontalSeam() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testRemoveVerticalSeam() {
+    fail("Not yet implemented");
+  }
+
+}
